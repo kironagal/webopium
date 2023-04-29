87b69383c6497b74dec0f85fb561b3d35f63a437
kishore <kishore4shivaraj@gmail.com>
2023-04-30 02:23:29 +0530

    ejs layout

--- projects/todolist/todolist-v1/app.js
+++ projects/todolist/todolist-v1/app.js
@@ -61,6 +61,10 @@ app.get("/work", function(req, res){
     });
 });
 
+//just to test EJS layouts by creating new /about route
+app.get("/about", function(req, res){
+    res.render("about");
+});
 
 app.listen(3000, function(){
     console.log("server listing on port 3000")
