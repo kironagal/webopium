8564cbfa2d1aa9048d5020a28529cee82f7bf916
kishore <kishore4shivaraj@gmail.com>
2023-05-10 03:15:01 +0530

    sessions cookies, login logout register secrets is is working

--- projects/Secrets - Authetication/app.js
+++ projects/Secrets - Authetication/app.js
@@ -3,15 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption'); // https://www.npmjs.com/package/mongoose-encryption
-const bcrypt = require('bcrypt'); // hashing with salting 
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose'); //since this package require "passport-local" we are not requiring it here
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+//adding express-session code
+app.use(session({
+    secret: 'OfficeHomeStudent',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Initalizing passport and passport session (middleware)
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
@@ -21,7 +32,16 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
+//Using Plugin Passport-Local Mongoose
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model('User', userSchema);
+
+//Simplified Passport/Passport-Local Configuration
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser()); //creats cookies
+passport.deserializeUser(User.deserializeUser()); // destroys cookies
 
 app.get("/", function (req, res){
     res.render('home');
@@ -35,41 +55,63 @@ app.get("/register", function (req, res){
     res.render('register');
 });
 
-app.post("/register", function(req, res){
-    // hashing with saltRounds and only passing it then to create user registration
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash){
-        const newUser = new User ({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save().then(function(saved){
-            res.render('secrets');
-        }).catch(function(err){
-            console.log(err);
-            res.render(err);
-        })
-    });
+app.get("/secrets", function (req, res){
+    if (req.isAuthenticated()){
+        res.render("secrets");
+    } else {
+        res.redirect("/register");
+    }
 });
 
-app.post("/login", function(req, res){
-    const userName = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: userName}).then(function (foundUser){
-        if (foundUser){
-            bcrypt.compare(password, foundUser.password, function (err, result){
-                if (result === true){
-                    res.render("secrets");
-                }
+app.post("/register", function(req, res){
+    User.register({username:req.body.username}, req.body.password, function (err, user){
+        if (err){
+            res.redirect("/register");
+        } else {
+            passport.authenticate('local') (req, res, function(){
+                res.redirect("/secrets");
             });
         }
-    }).catch(function(err){
-        console.log(err);
-        res.render(err);
     });
 });
 
+//Using login from passport https://www.passportjs.org/concepts/authentication/login/
+app.post("/login", function (req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if (err){
+            console.log(err);
+            res.send(err);
+        }else{
+            passport.authenticate('local') (req, res, function(){
+                res.redirect("/secrets"); 
+            });
+        }
+    });
+});
 
+//Using logout from passport https://www.passportjs.org/concepts/authentication/logout/
+app.get("/logout", function (req, res, next){
+    req.logOut(function (err){
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });   
+});
+
+// This part isn't working method not correct
+// app.post("/logout", function (req, res, next){
+//     req.logOut(function (err){
+//         if (err) {
+//             return next(err);
+//         }
+//         res.redirect("/");
+//     });
+// });
 
 app.listen(3000, function(){
     console.log("Server listening on port 3000");
